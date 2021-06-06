42d5871f9ea9c57431794dba1675c12bcdaf4d59
kamesm <73391430+kamesm@users.noreply.github.com>
2021-06-05 21:27:03 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -8,9 +8,16 @@ var drop = [];
 var umbrella;
 
 var thunder;
+var thunder1, thunder2, thunder3, thunder4;
+var randomnum;
+
+var thunderCreatedFrame;
 
 function preload(){
-   
+   thunder1 = loadImage("1.png");
+   thunder2 = loadImage("2.png");
+   thunder3 = loadImage("3.png");
+   thunder4 = loadImage("4.png");
 }
 
 function setup(){
@@ -26,8 +33,6 @@ function setup(){
 
 function draw(){ 
     background(0);
-    thunder = createSprite(random(0,350),random(10,20),10,10);
-    
 
     Engine.update(engine);
     for(var i=0; i<maxDrops;i++){
@@ -35,5 +40,34 @@ function draw(){
         drop[i].update();
     }    
     umbrella.display();
+
+    randomnum = Math.round(random(1,4));
+
+    if(frameCount % 80 === 0){
+        thunderCreatedFrame = frameCount;
+        thunder = createSprite(random(0,350),random(10,20),10,10);
+        thunder.scale = random(0.3,0.6);
+        switch(randomnum){
+        case 1: 
+            thunder.addImage(thunder1)  
+        break;
+        case 2: 
+            thunder.addImage(thunder2)  
+        break;
+        case 3: 
+            thunder.addImage(thunder3)  
+        break;
+        case 4: 
+            thunder.addImage(thunder4)  
+        break;
+        default: break;
+        }
+    }
+
+    if(thunderCreatedFrame + 10 === frameCount && thunder){
+        thunder.destroy();
+    }
+
+    drawSprites();
 }   
 
